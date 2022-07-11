095b549c9823d63da4f53e2628f3c505c04f196c
haris <haris@Hareesh>
2022-07-11 11:38:04 +0530

    Update data

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -1,8 +1,19 @@
 package test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Test {
 	public static void main(String[] args) {
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/ma","root","root");
+		String customerinsert="insert into customer_deatails values(111,'ramu','ramu@gmail.com','987654768','hyd')";
+		
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 		
 	}
 	
